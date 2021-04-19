8b10f7133d1ed7f900ee613b4f3246cec5c7140c
Le Bao Hiep <baohiep2013@gmail.com>
2021-04-19 15:51:44 +0700

    Stricter validators

--- src/v2/validators/common.ts
+++ src/v2/validators/common.ts
@@ -1,6 +1,29 @@
 import { Request, Response, NextFunction } from 'express';
-import { validationResult } from 'express-validator';
+import { CustomValidator, validationResult } from 'express-validator';
 import { ERRORS, LogicError } from '../errors';
+
+const isArrayOfObjects: CustomValidator = (value: any) => {
+  if (
+    !Array.isArray(value) ||
+    !value.every((item) => typeof item === 'object')
+  ) {
+    throw new Error('Not an array of objects');
+  }
+  return true;
+};
+
+const createFieldsValidator = (fields: Array<string>) => {
+  const containsOnlyFields: CustomValidator = (value: any) => {
+    if (typeof value !== 'object') {
+      throw new Error('Not an object');
+    }
+    if (!Object.keys(value).every((item) => fields.includes(item))) {
+      throw new Error('Object contains unknown field');
+    }
+    return true;
+  };
+  return containsOnlyFields;
+};
 
 function validationMiddleware(req: Request, res: Response, next: NextFunction) {
   const errors = validationResult(req);
@@ -11,4 +34,4 @@ function validationMiddleware(req: Request, res: Response, next: NextFunction) {
   }
 }
 
-export { validationMiddleware };
+export { isArrayOfObjects, createFieldsValidator, validationMiddleware };
--- src/v2/validators/participations.ts
+++ src/v2/validators/participations.ts
@@ -1,22 +1,36 @@
-import { body, param, query } from 'express-validator';
-import { validationMiddleware } from './common';
+import { body } from 'express-validator';
+import {
+  isArrayOfObjects,
+  createFieldsValidator,
+  validationMiddleware
+} from './common';
 
-const createParticipation = [body('values').isArray(), validationMiddleware];
+const createParticipation = [
+  body('values').isArray(),
+  body().custom(createFieldsValidator(['values'])),
+  validationMiddleware
+];
 
 const readParticipation = [
   body('offset').isInt({ min: 0 }),
   body('limit').isInt({ min: 0 }),
   body('where').isObject().optional(),
+  body().custom(createFieldsValidator(['offset', 'limit', 'where'])),
   validationMiddleware
 ];
 
 const updateParticipation = [
   body('where').isObject(),
-  body('values').isArray(),
+  body('values').custom(isArrayOfObjects),
+  body().custom(createFieldsValidator(['where', 'values'])),
   validationMiddleware
 ];
 
-const deleteParticipation = [body('where').isObject(), validationMiddleware];
+const deleteParticipation = [
+  body('where').isObject(),
+  body().custom(createFieldsValidator(['where'])),
+  validationMiddleware
+];
 
 export default {
   createParticipation,
