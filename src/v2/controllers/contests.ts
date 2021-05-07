af775436b5564e844223aa2448cb5ce171ea4113
quandang2002 <quanvda>
2021-05-07 22:11:11 +0700

    update contest

---
+++ src/v2/controllers/contests.ts
@@ -0,0 +1,94 @@
+import { Request, Response, NextFunction } from 'express';
+import { RequestBodySchema } from '../schemas';
+import knex from '../../db';
+
+const createContest = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const value = req.body as RequestBodySchema;
+        await knex('Contests').insert(value);
+
+        res.json({
+            error: 0
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+
+const readContest = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { offset, limit } = req.body as RequestBodySchema;
+        const where = req.body?.where || {};
+        const items = await knex('Contests')
+            .select('*')
+            .where(where)
+            .offset(offset)
+            .limit(limit);
+
+        res.json({
+            error: 0,
+            data: {
+                total: items.length,
+                items
+            }
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const updateContest = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { where, values } = req.body as RequestBodySchema;
+        const query = knex('Contests').where(where);
+        if (Array.isArray(values)) {
+            for (const value of values) {
+                query.update(value);
+            }
+        }
+        await query;
+
+        res.json({
+            error: 0
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const deleteContest = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { where } = req.body as RequestBodySchema;
+        await knex('Contest').where(where).del();
+
+        res.json({
+            error: 0
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
+export default {
+    createContest,
+    readContest,
+    updateContest,
+    deleteContest
+}
