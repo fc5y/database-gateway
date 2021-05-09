37f12c4c0a1bb40c39150aad8bb85d1d4cc111fa
quandang2002 <quanvda>
2021-05-09 23:37:07 +0700

    prettier:

--- src/v2/controllers/contests.ts
+++ src/v2/controllers/contests.ts
@@ -1,93 +1,84 @@
-import { Request, Response, NextFunction } from 'express';
-import { RequestBodySchema } from '../schemas';
-import knex from '../../db';
+import { Request, Response, NextFunction } from "express";
+import { RequestBodySchema } from "../schemas";
+import knex from "../../db";
 
 const createContest = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {
-        const value = req.body as RequestBodySchema;
-        await knex('Contests').insert(value);
+  try {
+    const value = req.body as RequestBodySchema;
+    await knex("Contests").insert(value);
 
-        res.json({
-            error: 0
-        });
-    } catch (err) {
-        next(err);
-    }
-}
+    res.json({
+      error: 0,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
+const readContest = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { offset, limit } = req.body as RequestBodySchema;
+    const where = req.body?.where || {};
+    const items = await knex("Contests")
+      .select("*")
+      .where(where)
+      .offset(offset)
+      .limit(limit);
+    const total = await knex("Contests").where(where).count("*");
 
-const readContest = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    try {
-        const { offset, limit } = req.body as RequestBodySchema;
-        const where = req.body?.where || {};
-        const items = await knex('Contests')
-            .select('*')
-            .where(where)
-            .offset(offset)
-            .limit(limit);
-        const total = await knex("Contests")
-            .where(where)
-            .count("*");
-
-        res.json({
-            error: 0,
-            data: {
-                total: total[0]["count(*)"],
-                items
-            }
-        });
-    } catch (err) {
-        next(err);
-    }
+    res.json({
+      error: 0,
+      data: {
+        total: total[0]["count(*)"],
+        items,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 const updateContest = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {
-        const { where, values } = req.body as RequestBodySchema;
-        await knex('Contests')
-            .where(where)
-            .update(values);
+  try {
+    const { where, values } = req.body as RequestBodySchema;
+    await knex("Contests").where(where).update(values);
 
-        res.json({
-            error: 0
-        });
-    } catch (err) {
-        next(err);
-    }
+    res.json({
+      error: 0,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 const deleteContest = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    try {
-        const { where } = req.body as RequestBodySchema;
-        await knex('Contest').where(where).del();
+  try {
+    const { where } = req.body as RequestBodySchema;
+    await knex("Contest").where(where).del();
 
-        res.json({
-            error: 0
-        });
-    } catch (err) {
-        next(err);
-    }
+    res.json({
+      error: 0,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 export default {
-    createContest,
-    readContest,
-    updateContest,
-    deleteContest
-}
+  createContest,
+  readContest,
+  updateContest,
+  deleteContest,
+};
--- src/v2/validators/contests.ts
+++ src/v2/validators/contests.ts
@@ -1,26 +1,26 @@
-import { body } from 'express-validator';
-import { validationMiddleware } from './common';
+import { body } from "express-validator";
+import { validationMiddleware } from "./common";
 
-const createContest = [
-    body('values').isObject(),
-    validationMiddleware
-]
+const createContest = [body("values").isObject(), validationMiddleware];
 
 const readContest = [
-    body('offset').isInt({ min: 0 }),
-    body('limit').isInt({ min: 0 }),
-    body('where').isObject().optional(),
-    validationMiddleware
-]
-
-const updateContest = [
-    body('where').isObject(),
-    body('values').isObject(),
-    validationMiddleware
+  body("offset").isInt({ min: 0 }),
+  body("limit").isInt({ min: 0 }),
+  body("where").isObject().optional(),
+  validationMiddleware,
 ];
 
-const deleteContest = [body('where').isObject(), validationMiddleware];
+const updateContest = [
+  body("where").isObject(),
+  body("values").isObject(),
+  validationMiddleware,
+];
+
+const deleteContest = [body("where").isObject(), validationMiddleware];
 
 export default {
-    createContest, readContest, updateContest, deleteContest
-}
+  createContest,
+  readContest,
+  updateContest,
+  deleteContest,
+};
