4b354d8f3b10b7eefcb6c2039b548955c15edde8
quandang2002 <quanvda>
2021-05-09 22:00:57 +0700

    update contests

---
+++ src/v2/validators/contests.ts
@@ -0,0 +1,26 @@
+import { body } from 'express-validator';
+import { validationMiddleware } from './common';
+
+const createContest = [
+    body('values').isObject(),
+    validationMiddleware
+]
+
+const readContest = [
+    body('offset').isInt({ min: 0 }),
+    body('limit').isInt({ min: 0 }),
+    body('where').isObject().optional(),
+    validationMiddleware
+]
+
+const updateContest = [
+    body('where').isObject(),
+    body('values').isObject(),
+    validationMiddleware
+];
+
+const deleteContest = [body('where').isObject(), validationMiddleware];
+
+export default {
+    createContest, readContest, updateContest, deleteContest
+}
