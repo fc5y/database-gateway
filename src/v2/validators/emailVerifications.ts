6afebcd98d0f3f235a3ee9ba7afdc52372787c74
minhhuylqd <minhhuy2810@gmail.com>
2021-05-10 12:23:19 +0700

    CRUD emailVerifications

---
+++ src/v2/validators/emailVerifications.ts
@@ -0,0 +1,32 @@
+import { body } from "express-validator";
+import { validationMiddleware } from "./common";
+
+const createEmailVerification = [
+  body("values").isObject(),
+  validationMiddleware,
+];
+
+const readEmailVerification = [
+  body("offset").isInt({ min: 0 }),
+  body("limit").isInt({ min: 0 }),
+  body("where").isObject().optional(),
+  validationMiddleware,
+];
+
+const updateEmailVerification = [
+  body("where").isObject(),
+  body("values").isObject(),
+  validationMiddleware,
+];
+
+const deleteEmailVerification = [
+  body("where").isObject(),
+  validationMiddleware,
+];
+
+export default {
+  createEmailVerification,
+  readEmailVerification,
+  updateEmailVerification,
+  deleteEmailVerification,
+};
