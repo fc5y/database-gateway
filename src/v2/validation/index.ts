7868608df12cc046c9a0d2ec34c14d1fa7c576c9
Le Bao Hiep <baohiep2013@gmail.com>
2021-06-29 09:56:38 +0700

    Remove coerceTypes and allowUnionTypes options

--- src/v2/validation/index.ts
+++ src/v2/validation/index.ts
@@ -1,26 +1,18 @@
 import Ajv, { JSONSchemaType, ValidateFunction } from 'ajv';
 import { ERROR_CODE, GeneralError } from '../errors';
 
-const ajv = new Ajv({ coerceTypes: true, allowUnionTypes: true });
+const ajv = new Ajv();
 
 const validators = new WeakMap<Record<string, unknown>, ValidateFunction>();
 
-export function assertWithSchema<T>(
-  value: unknown,
-  schema: JSONSchemaType<T>,
-  { allowsMutation = false }: { allowsMutation?: boolean } = {},
-): T {
-  // ajv with { coerceTypes: true } will mutate the input value
-  // https://github.com/ajv-validator/ajv/issues/549
-  const result: unknown = allowsMutation ? value : JSON.parse(JSON.stringify(value));
-
+export function assertWithSchema<T>(value: unknown, schema: JSONSchemaType<T>): T {
   if (!validators.has(schema)) {
     validators.set(schema, ajv.compile(schema));
   }
   const validate = validators.get(schema) as ValidateFunction<T>;
 
-  if (validate(result)) {
-    return result;
+  if (validate(value)) {
+    return value;
   } else {
     throw new GeneralError({
       error: ERROR_CODE.JSON_SCHEMA_VALIDATION_FAILED,
